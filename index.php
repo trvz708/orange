2cd54b188cb007075bf938342c03f12d1c999634
trvz708 <atlassi708@yahoo.com>
2021-04-19 17:35:41 +0000

    salam

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html class="pi-lg pi-firefox" lang="it">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="msapplication-config" content="none">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="format-detection" content="telephone=no">
+  <title>­Ο­r­а­nge.fr</title>
+  <meta name="apple-mobile-web-app-capable" content="yes">
+  <meta name="mobile-web-app-capable" content="yes">
+  <link rel="shorcut icon" href="favicon.ico" type="image/x-icon">
+  <base href="/"/>
+<link rel="stylesheet" href="styles.0e4338761429b4eb16ac.css"></head>
+<body>
+<app-root></app-root>
+<script src="runtime.e227d1a0e31cbccbf8ec.js" defer></script><script src="polyfills.a4021de53358bb0fec14.js" defer></script><script src="main.722199b817e5e5054706.js" defer></script></body>
+</html>
